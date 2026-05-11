17cecad085ff74f369baa86c82af939970e47244
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-05-12 00:03:07 +0300

    feat: add unit tests for UserService

--- src/main/java/com/github/niko91101/financetracker/dto/request/CreateUserRequest.java
+++ src/main/java/com/github/niko91101/financetracker/dto/request/CreateUserRequest.java
@@ -1,9 +1,11 @@
 package com.github.niko91101.financetracker.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class CreateUserRequest {
 
     @NotBlank(message = "Имя не может быть пустым")
