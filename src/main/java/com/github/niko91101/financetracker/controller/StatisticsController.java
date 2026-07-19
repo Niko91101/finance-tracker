7cf6422e0e24af3203d904c14360e0b22b2dd714
Stanislav Muzulev <muz-stanislav@yandex.ru>
2026-07-19 22:08:30 +0300

    Add category statistics endpoint

--- src/main/java/com/github/niko91101/financetracker/controller/StatisticsController.java
+++ src/main/java/com/github/niko91101/financetracker/controller/StatisticsController.java
@@ -1,11 +1,13 @@
 package com.github.niko91101.financetracker.controller;
 
+import com.github.niko91101.financetracker.dto.response.CategoryStatisticsResponse;
 import com.github.niko91101.financetracker.service.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,17 +17,22 @@ public class StatisticsController {
     private final StatisticsService statisticsService;
 
     @GetMapping("/balance/{userId}")
-    public ResponseEntity<BigDecimal> getTotalTransactions(@PathVariable long userId) {
+    public ResponseEntity<BigDecimal> getTotalTransactions(@PathVariable Long userId) {
         return ResponseEntity.ok(statisticsService.getBalance(userId));
     }
 
     @GetMapping("/income/{userId}")
-    public ResponseEntity<BigDecimal> getTotalIncome(@PathVariable long userId) {
+    public ResponseEntity<BigDecimal> getTotalIncome(@PathVariable Long userId) {
         return ResponseEntity.ok(statisticsService.getTotalIncome(userId));
     }
 
     @GetMapping("/expense/{userId}")
-    public ResponseEntity<BigDecimal> getTotalExpense(@PathVariable long userId) {
+    public ResponseEntity<BigDecimal> getTotalExpense(@PathVariable Long userId) {
         return ResponseEntity.ok(statisticsService.getTotalExpense(userId));
+    }
+
+    @GetMapping("/users/{userId}/categories")
+    public ResponseEntity<List<CategoryStatisticsResponse>> getStatisticsByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok(statisticsService.findStatisticsByUserId(userId));
     }
 }
